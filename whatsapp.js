baaa6f52592a3e8a65e21249422441a3cbb3d1f9
ahmadabbar <ahmadabodyaa121@gmail.com>
2024-10-27 00:23:27 +0300

    responsive1234567

--- whatsapp.js
+++ whatsapp.js
@@ -14,12 +14,22 @@ function sendToWhatsapp() {
         let firstName = document.getElementById('firstName').value;
         let lastName = document.getElementById('lastName').value;
         let email = document.getElementById('email').value;
+        let country = document.getElementById('country').value;
         let message = document.getElementById('message').value;
         let url = `https://wa.me/${phoneNumber}?text=السلام عليكم ورحمة الله وبركاته 
     اسمي ${firstName} ${lastName} 
-    والبريد الإلكتروني ${email}. 
+        والبريد الإلكتروني ${email}. 
+        والدولة ${country}.
      وأريد أن أقول: ${message}`;
         window.open(url, '_blank').focus();
+    
+}
+function whatsappsupport(){
+    let phoneNumber = '+352681588623';
+    let message = 'السلام عليكم ورحمة الله وبركاته كيف حالك أستاذ أحمد عبار أريد الاستفسار عن الخدمات التي تقدمها في مجال برمجة مواقع الويب';
+    let url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, '_blank').focus();
+   
     
 }
 
